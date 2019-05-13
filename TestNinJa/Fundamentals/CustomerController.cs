6a6ac0d6efc9426708fd6080c1856100a32afce6
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-13 14:25:49 +0700

    testing return type

---
+++ TestNinJa/Fundamentals/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿namespace TestNinJa.Fundamentals
+{
+    public class CustomerController
+    {
+        public ActionResult GetCustomer(int id)
+        {
+            if (id == 0)
+                return new NotFound();
+            return new OK();
+        }
+    }
+
+    public class OK : ActionResult
+    {
+    }
+
+    public class NotFound : ActionResult
+    {
+    }
+
+    public class ActionResult
+    {
+    }
+}
---
+++ TestNinja.UnitTest/CustomerControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using TestNinJa.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            //Not Found
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            //Not Found or one of its derivatives
+
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+    }
+}
