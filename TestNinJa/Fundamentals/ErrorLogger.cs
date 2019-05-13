61d1bea08386ca546a06c7a52ac6c5353d634f4d
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-13 14:41:26 +0700

    Void method testing

---
+++ TestNinJa/Fundamentals/ErrorLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestNinJa.Fundamentals
+{
+    public class ErrorLogger
+    {
+        public string LastError { get; set; }
+
+        public event EventHandler<Guid> ErrorLogged;
+
+        public void Log(string error)
+        {
+            if (string.IsNullOrWhiteSpace(error))
+                throw new ArgumentNullException();
+            LastError = error;
+
+            ErrorLogged?.Invoke(this, Guid.NewGuid());
+        }
+    }
+}
---
+++ TestNinja.UnitTest/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinJa.Fundamentals;
+
+namespace TestNinja.UnitTest
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+            logger.Log("a");
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidError_ThrowArgumentNullException(string error)
+        {
+            var logger = new ErrorLogger();
+
+            Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
+        }
+    }
+}
