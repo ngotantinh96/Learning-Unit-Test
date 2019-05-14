d2b2999178e49cd08f259161ab3bc9757ae4e0c4
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-14 10:59:51 +0700

    setup mock

--- TestNinja.UnitTest/Mocking/VideoServiceTests.cs
+++ TestNinja.UnitTest/Mocking/VideoServiceTests.cs
@@ -7,12 +7,18 @@ namespace TestNinja.UnitTest.Mocking
     [TestFixture]
     public class VideoServiceTests
     {
+        private Mock<IFileReader> fileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            fileReader = new Mock<IFileReader>();
+            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var fileReader = new Mock<IFileReader>();
-            fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
-
             var service = new VideoService(fileReader.Object);
             var result = service.ReadVideoTitle();
             Assert.That(result, Does.Contain("error").IgnoreCase);
