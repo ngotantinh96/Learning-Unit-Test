a12edc0bb9cb6fa9aa7d2d00955879f7721e7183
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-05-20 09:22:41 +0700

    abuse mock test

---
+++ TestNinja.UnitTest/Mocking/ProductTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using TestNinJa.Mocking;
+
+namespace TestNinja.UnitTest.Mocking
+{
+    [TestFixture]
+    public class ProductTests
+    {
+        [Test]
+        public void GetPrice_GoldCustomer_Apply30PercentDiscount()
+        {
+            var product = new Product { ListPrice = 100 };
+            var result = product.GetPrice(new Customer { IsGold = true });
+            Assert.That(result, Is.EqualTo(70));
+        }
+    }
+}
